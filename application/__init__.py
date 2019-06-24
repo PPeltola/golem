a2020e6932853b991ef3a239527ab3b3029ae552
PPeltola <murderbanana@gmail.com>
2019-06-24 14:43:28 +0300

    indentation fix

--- application/__init__.py
+++ application/__init__.py
@@ -7,8 +7,8 @@ import os
 if os.environ.get("HEROKU"):
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 else:
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///golem.db"
-app.config["SQLALCHEMY_ECHO"] = True
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///golem.db"
+    app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
